6f3c8068ec42debb6aea4c991c8f07c4293dde2e
Justin Coyne <jcoyne@justincoyne.com>
2020-09-21 13:06:28 -0500

    Minimal test

--- spec/requests/resources_request_spec.rb
+++ spec/requests/resources_request_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 RSpec.describe "Resources", type: :request do
   let(:data) do
     <<~JSON
-      {
-      "label":"7116",
-      "version":2,
-      "description":{"title":[{"value":"7116"}]},
-      "identification":{"sourceId":"sul:naxos_7116"}}
+      { "title":{"value":"7116"}}
     JSON
   end
   it 'updates the object' do
